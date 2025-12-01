b47172214dd97efb46050051ddfcef9b56f709c8
esobirjon2197 <esobirjon2197@gmail.com>
2025-12-01 15:03:50 +0500

    Add string slicing examples in main.py

---
+++ main.py
@@ -0,0 +1,85 @@
+# 1
+i = "mathemtics"
+print(i)
+
+t = i[0:4]
+print(t)
+
+
+
+# 2
+i = "developament"
+print(i)
+
+t = i[0:6]
+print(t)
+
+
+# 3
+i = "progarminglanguage"
+print(i)
+
+t = i[0:8]
+print(t)
+
+
+
+# 4
+i = "absdefghih"
+print(print(i))
+
+t = i[::]
+print(t)
+
+
+
+# 5
+i = "123456789"
+print(i)
+
+t = i[2:3]
+print(t)
+
+
+# 6
+i = "HelloPythonWorld"
+print(i)
+
+t = i[5:11]
+print(t)
+
+
+# 7
+i = "abdefghjk"
+print(i)
+
+t = i[3:8]
+print(t)
+
+
+# 8
+i = "superdevelob"
+print(i)
+
+t = i[:-5]
+print(t)
+
+
+# 9
+i = "superdevelob"
+print(i)
+
+t = i[:-5]
+print(t)
+
+
+# 10
+i = "123456789"
+print(i)
+
+t = i[0:9:2]
+print(t)
+
+
+
+
